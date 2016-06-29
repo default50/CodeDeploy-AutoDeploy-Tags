b012e3822189d4fdcc90c186e1d77af3086885cc
Sebastian Cruz <cruzseba@amazon.com>
2016-06-29 17:29:57 +0100

    WiP. Some clean up and moved to .format for print

--- autodeploy-prj/autodeploy-app/autodeploy-lambda/autodeploy.py
+++ autodeploy-prj/autodeploy-app/autodeploy-lambda/autodeploy.py
@@ -18,6 +18,7 @@ cd_dg_name = "Demo-Tag-Ubuntu-PreProd"
 # Replace the following tag Key and Value for the one used in your initial Deployment Group
 cd_dg_tag = {'Key': 'CodeDeploy', 'Value': 'PreProd'}
 
+
 # Function to merge filtered dictionaries into one. Unmatched keys allowed.
 # For each key specified values will become a list of values in the order
 # of *args. Empty values of keys become None in the resulting list.
@@ -29,28 +30,9 @@ def dict_merger(key_filters=None, *args):
 
     return dict(dd)
 
-# Function to deal with paginated results. Have to generalize it. Returns a generator.
-def cd_get_deployments(client, token=None, **params):
-    #print token
-    #for k,v in params.iteritems():
-    #         print "%s = %s" % (k, v)
-
-    results = None
-    if token:
-        results = client.list_deployments(nextToken=token, **params)
-    else:
-        results = client.list_deployments(**params)
-
-    for i in results['deployments']:
-        yield i
-
-    if 'nextToken' in results:
-        for i in cd_get_deployments(client, token=results['nextToken'], **params):
-            yield i
 
 # Function to deal with paginated results.
 def get_all_results(client_function, key_filters, **params):
-
     tmpResults = [client_function(**params)]
 
     while tmpResults[-1].get('nextToken'):
@@ -59,6 +41,7 @@ def get_all_results(client_function, key_filters, **params):
     results = dict_merger(key_filters, *tmpResults)
 
     return results
+
 
 def autodeploy_handler(event, context):
     #print ("Received event dump:")
@@ -71,24 +54,24 @@ def autodeploy_handler(event, context):
     ec2_client = boto3.client('ec2', region_name=event['region'])
     cd = boto3.client('codedeploy', region_name=event['region'])
        
-    print "Event Region:", event['region']
-    print "Event Time:", event['time']
-    print "Instance ID:", event['detail']['instance-id']
-    print "Instance Status:", event['detail']['state']
+    print "Event Region: {0}".format(event['region'])
+    print "Event Time: {0}".format(event['time'])
+    print "Instance ID: {0}".format(event['detail']['instance-id'])
+    print "Instance Status: {0}".format(event['detail']['state'])
     
     # Obtain the EC2 object of the instance from the event
     instance = ec2.Instance(event['detail']['instance-id'])
 
     # Print a dict of the Tags (see https://github.com/boto/boto3/issues/264)
-    print "Tags:", dict(map(lambda x: (x['Key'], x['Value']), instance.tags or []))
+    print "Tags: {0}".format(dict(map(lambda x: (x['Key'], x['Value']), instance.tags or [])))
 
     # Print a dict of the filtering Tag
-    print "Filter: {'%s': '%s'}" % (cd_dg_tag['Key'], cd_dg_tag['Value'])
+    print "Filter: {{'{0}': '{1}'}}".format(cd_dg_tag['Key'], cd_dg_tag['Value'])
 
     if instance.tags is not None:
         for t in instance.tags:
             if t['Key'] == cd_dg_tag['Key'] and t['Value'] == cd_dg_tag['Value']:
-                print "Instance %s is a target for AutoDeploy!" % instance.id
+                print "Instance {0} is a target for AutoDeploy!".format(instance.id)
 
     deployments = get_all_results(
         cd.list_deployments,
@@ -99,8 +82,7 @@ def autodeploy_handler(event, context):
         )
     # Flatten the resulting list
     deployments = [item for sublist in deployments['deployments'] for item in sublist]
-    print(deployments)
-    #pp.pprint(deployments)
+    pp.pprint(deployments)
 
     #print "----------> instances results below:"
     #instances = get_all_results(
